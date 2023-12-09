708a54d5c463fc94460930fd6a46be6009aede27
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-12-08 20:08:15 -0500

    Se inicia el metodo para editar clientes.

--- CRM/crm-react/src/data/clientes.js
+++ CRM/crm-react/src/data/clientes.js
@@ -5,6 +5,13 @@ export async function obtenerClientes(){
     return result;
 }
 
+export async function obtenerCliente(id){
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/${id}`);
+    const result = await response.json();
+
+    return result;
+}
+
 export async function agregarCliente(datos){
 
     try {
